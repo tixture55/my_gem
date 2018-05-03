1ae800f67f85adcf322abd7495ff85dc72c9d2e2
frank marks <hdx48s@yahoo.co.jp>
2018-05-03 22:12:45 +0900

    gem for rspec added

--- lib/my_rubygem.rb
+++ lib/my_rubygem.rb
@@ -1,5 +1,16 @@
 require "my_rubygem/version"
+require "my_rubygem/configuration"
+require "my_rubygem/base"
+
 
 module MyRubygem
-  # Your code goes here...
+  class << self
+    def configure
+      yeild configuration
+    end
+
+    def configuration
+      @configuration ||= MyRubygem::Configuration.new
+    end
+  end
 end
---
+++ lib/my_rubygem/configuration.rb
@@ -0,0 +1,3 @@
+class MyRubygem::Configuration
+  attr_accessor :access_token
+end
